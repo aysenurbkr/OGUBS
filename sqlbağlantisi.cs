8a6a08437223f7abb0145dc76767477239f1f032
ayşenur <aysenurbkr58@gmail.com>
2024-12-12 23:17:28 +0300

    Add files via upload

---
+++ sqlbağlantisi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp12
+{
+    internal class sqlbağlantisi
+    {
+        public SqlConnection baglanti()
+        {
+            SqlConnection baglan = new SqlConnection("Data Source=LAPTOP-U1IT6GI8;Initial Catalog=OgrenciIsleri;Integrated Security=True;");
+            baglan.Open();
+            return baglan;    
+        }
+    }
+}
